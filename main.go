92aebc39ca2262f20b086b0b86626bd9aa32d4d4
SchnilchMitte <wohgsfalu@gmail.com>
2026-09-08 20:24:54 +0200

    rework to make both stats available

--- main.go
+++ main.go
@@ -11,98 +11,217 @@ import (
 )
 
 // We only allow one measurement at a time.
-var dockerStats *exec.Cmd
+
+var kafkaStats *exec.Cmd
+var natsStats *exec.Cmd
 var sar *exec.Cmd
-var containerFile *os.File
+
+var kafkaFile *os.File
+var natsFile *os.File
 var hostFile *os.File
+
 var resultDir string
 
 func startMeasurement(w http.ResponseWriter, r *http.Request) {
-	if dockerStats != nil {
+	if kafkaStats != nil || natsStats != nil {
 		http.Error(w, "measurement already running", 400)
 		return
 	}
 
 	container := r.URL.Query().Get("container")
-	if container != "kafka" && container != "nats" {
-		http.Error(w, "good try", 400)
+
+	if container != "kafka" &&
+		container != "nats" &&
+		container != "both" {
+
+		http.Error(w, "container must be kafka, nats or both", 400)
 		return
 	}
 
 	startTime := time.Now()
-	resultDir = filepath.Join("measurements", container+"-"+startTime.Format("20060102-150405"))
-	os.MkdirAll(resultDir, 0755)
 
-	containerFile, _ = os.Create(filepath.Join(resultDir, "container.csv"))
-	hostFile, _ = os.Create(filepath.Join(resultDir, "host.txt"))
+	resultDir = filepath.Join(
+		"measurements",
+		container+"-"+startTime.Format("20060102-150405"),
+	)
 
-	fmt.Fprintln(containerFile, "timestamp,name,cpu,mem_usage,mem_percent,net_io,block_io,pids")
-	fmt.Fprintf(hostFile, "measurement_started=%s\n\n", startTime.Format(time.RFC3339Nano))
+	if err := os.MkdirAll(resultDir, 0755); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 
-	// Take one docker stats sample every second and add our own timestamp.
-	// The container name is safe because we only accept "kafka" or "nats" above.
-
-	// Start continuous Docker stats stream.
 	var err error
 
-	dockerStats, err = startDockerStats(container, containerFile)
+	// -------------------------
+	// Kafka measurement
+	// -------------------------
+
+	if container == "kafka" || container == "both" {
+		kafkaFile, err = os.Create(
+			filepath.Join(resultDir, "kafka.csv"),
+		)
+		if err != nil {
+			cleanup()
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		fmt.Fprintln(
+			kafkaFile,
+			"timestamp,name,cpu,mem_usage,mem_percent,net_io,block_io,pids",
+		)
+
+		kafkaStats, err = startDockerStats(
+			"kafka",
+			kafkaFile,
+		)
+		if err != nil {
+			cleanup()
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+
+	// -------------------------
+	// NATS measurement
+	// -------------------------
+
+	if container == "nats" || container == "both" {
+		natsFile, err = os.Create(
+			filepath.Join(resultDir, "nats.csv"),
+		)
+		if err != nil {
+			cleanup()
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		fmt.Fprintln(
+			natsFile,
+			"timestamp,name,cpu,mem_usage,mem_percent,net_io,block_io,pids",
+		)
+
+		natsStats, err = startDockerStats(
+			"nats",
+			natsFile,
+		)
+		if err != nil {
+			cleanup()
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+
+	// -------------------------
+	// Host measurement
+	// -------------------------
+
+	hostFile, err = os.Create(
+		filepath.Join(resultDir, "host.txt"),
+	)
 	if err != nil {
 		cleanup()
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	// Measure CPU, RAM, network and disk of the complete server every second.
-	sar = exec.Command("sar", "-u", "-r", "-d", "1")
+	fmt.Fprintf(
+		hostFile,
+		"measurement_started=%s\n\n",
+		startTime.Format(time.RFC3339Nano),
+	)
+
+	sar = exec.Command(
+		"sar",
+		"-u",
+		"-r",
+		"-d",
+		"1",
+	)
+
 	sar.Stdout = hostFile
 	sar.Stderr = hostFile
 
 	if err := sar.Start(); err != nil {
-		_ = dockerStats.Process.Kill()
-		_ = dockerStats.Wait()
 		cleanup()
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	fmt.Fprintf(w, "started %s measurement\nstart: %s\nresults: %s\n",
-		container, startTime.Format(time.RFC3339Nano), resultDir)
+	fmt.Fprintf(
+		w,
+		"started %s measurement\nstart: %s\nresults: %s\n",
+		container,
+		startTime.Format(time.RFC3339Nano),
+		resultDir,
+	)
 }
 
 func stopMeasurement(w http.ResponseWriter, r *http.Request) {
-	if dockerStats == nil {
+	if kafkaStats == nil && natsStats == nil {
 		http.Error(w, "no measurement running", 400)
 		return
 	}
 
 	stopTime := time.Now()
 
-	_ = dockerStats.Process.Kill()
-	_ = sar.Process.Kill()
-	_ = dockerStats.Wait()
-	_ = sar.Wait()
+	if kafkaStats != nil {
+		_ = kafkaStats.Process.Kill()
+		_ = kafkaStats.Wait()
+	}
 
-	fmt.Fprintf(hostFile, "\nmeasurement_stopped=%s\n", stopTime.Format(time.RFC3339Nano))
+	if natsStats != nil {
+		_ = natsStats.Process.Kill()
+		_ = natsStats.Wait()
+	}
+
+	if sar != nil {
+		_ = sar.Process.Kill()
+		_ = sar.Wait()
+	}
+
+	if hostFile != nil {
+		fmt.Fprintf(
+			hostFile,
+			"\nmeasurement_stopped=%s\n",
+			stopTime.Format(time.RFC3339Nano),
+		)
+	}
 
 	dir := resultDir
+
 	cleanup()
 
-	fmt.Fprintf(w, "measurement stopped\nstop: %s\nresults: %s\n",
-		stopTime.Format(time.RFC3339Nano), dir)
+	fmt.Fprintf(
+		w,
+		"measurement stopped\nstop: %s\nresults: %s\n",
+		stopTime.Format(time.RFC3339Nano),
+		dir,
+	)
 }
 
 func cleanup() {
-	if containerFile != nil {
-		containerFile.Close()
+	if kafkaFile != nil {
+		kafkaFile.Close()
 	}
+
+	if natsFile != nil {
+		natsFile.Close()
+	}
+
 	if hostFile != nil {
 		hostFile.Close()
 	}
 
-	dockerStats = nil
+	kafkaStats = nil
+	natsStats = nil
 	sar = nil
-	containerFile = nil
+
+	kafkaFile = nil
+	natsFile = nil
 	hostFile = nil
+
+	resultDir = ""
 }
 
 func startDockerStats(container string, outputFile *os.File) (*exec.Cmd, error) {
@@ -111,6 +230,7 @@ while true; do
 	docker stats --no-stream \
 		--format '{{.Name}},{{.CPUPerc}},{{.MemUsage}},{{.MemPerc}},{{.NetIO}},{{.BlockIO}},{{.PIDs}}' \
 		%s
+		sleep 0.3
 done
 `, container)
 
